a69138e65259dabde63eb72970dc7279954f48c9
ItumelengMphuti <125954886+ItumelengMphuti@users.noreply.github.com>
2023-10-20 15:51:43 +0200

    new changes

---
+++ main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    var scrollToTopButton = document.getElementById("scroll-to-top");
+    
+    window.addEventListener("scroll", function () {
+       if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                scrollToTopButton.style.display = "block";
+        } else {
+            scrollToTopButton.style.display = "none";
+          }
+      });
+
+      scrollToTopButton.addEventListener("click", function () {
+        window.scrollTo({
+         top: 0,
+         behavior: "smooth"
+      });
+    });
+});
